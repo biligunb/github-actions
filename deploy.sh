46433f60c2fe65390d02ed1047a144cd641ce329
biligunb <biligunb@gmail.com>
2022-01-31 12:37:16 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 echo "Kill all the running PM2 actions"
-sudo pm2 kill
+whoami
+pm2 kill
 
 echo "Jump to app folder"
 cd /home/ubuntu/github-actions
@@ -9,7 +10,7 @@ git pull
 
 echo "Install app dependencies"
 sudo rm -rf node_modules package-lock.json
-sudo npm install
+npm install
 
 echo "Run new PM2 action"
-sudo pm2 start ecosystem.config.js
+pm2 start ecosystem.config.js
