1a22df28c523b063654ea2ea59245920d4e9e4e7
biligunb <biligunb@gmail.com>
2022-01-31 12:04:56 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Return all todos')
+})
+
+app.post('/add', (req, res) => {
+  console.log('Request: ', req.body)
+  res.send('Successfully added new todo')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+module.exports = app;
